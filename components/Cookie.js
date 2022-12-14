0312838f4d66a54d7b97dca5ccab962a93d2a2f5
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-14 15:54:37 -0600

    Names go to uppercase using JS

--- components/Cookie.js
+++ components/Cookie.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cookie = (props) => {
     return( // JSX goes inside here
         <div>
-            <h2>Have {props.numberOfCookies} cookies, {props.name}, because you are great!</h2>
+            <h2>Have {props.numberOfCookies} cookies, { props.name.toUpperCase() }, because you are great!</h2>
         </div>
     )
 };
